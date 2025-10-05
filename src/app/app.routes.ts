057ea0e7452eb11f0512ee14d6a63e61ac2e6708
raedzayoud <raed.zayoud.23@gmail.com>
2025-10-05 17:51:52 +0200

    build the menu of landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/auth/login/login';
+import { Menu } from './pages/menu/menu';
+import { Landingpage } from './pages/landingpage/landingpage';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'landingpage', pathMatch: 'full' },
 
   { path: 'login', component: Login },
+  { path: 'menu', component: Menu },
+  { path: 'landingpage', component: Landingpage },
 ];
---
+++ src/app/pages/landingpage/landingpage.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Menu } from '../menu/menu';
+
+@Component({
+  selector: 'app-landingpage',
+  imports: [Menu],
+  templateUrl: './landingpage.html',
+  styleUrl: './landingpage.css',
+})
+export class Landingpage {}
