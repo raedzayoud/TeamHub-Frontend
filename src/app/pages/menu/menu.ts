173a2e65a21d63bd6c1bf11e237fdcff4edf7e6c
raedzayoud <raed.zayoud.23@gmail.com>
2025-10-06 12:08:03 +0200

    end landing page ui

--- src/app/pages/menu/menu.ts
+++ src/app/pages/menu/menu.ts
@@ -4,8 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-menu',
   imports: [],
   templateUrl: './menu.html',
-  styleUrl: './menu.css'
+  styleUrl: './menu.css',
 })
 export class Menu {
-
+  scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
