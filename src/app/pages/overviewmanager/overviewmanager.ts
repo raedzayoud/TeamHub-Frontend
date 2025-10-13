a5002bc31a169376bb89157a17d0f8f599cc6f17
raedzayoud <raed.zayoud.23@gmail.com>
2025-10-13 12:04:51 +0200

    add ui add project

--- src/app/pages/overviewmanager/overviewmanager.ts
+++ src/app/pages/overviewmanager/overviewmanager.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-overviewmanager',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './overviewmanager.html',
-  styleUrl: './overviewmanager.css'
+  styleUrl: './overviewmanager.css',
 })
 export class Overviewmanager {
+  isShown: boolean = false;
 
+  toggleShow() {
+    this.isShown = !this.isShown;
+  }
 }
